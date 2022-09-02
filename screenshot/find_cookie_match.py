5c577c8d657fdde5917273d85e38f6c959fd5c9f
Julius Eller <juliuseller@Juliuss-MacBook-Pro.fritz.box>
2022-09-02 11:26:53 +0200

    Refactoring of finding cookie

--- screenshot/find_cookie_match.py
+++ screenshot/find_cookie_match.py
@@ -1,6 +1,7 @@
-def find_cookie_match(button_text):
+from selenium.webdriver.common.by import By
+def accept_match(button_text):
     cookie_shorthand_matches = [
-        ["Accept", "Agree", "Allow", "Approve", "Continue", "Got it", "I agree", "I accept", "I understand", "OK", "Okay", "Save preferences", "Save settings", "Save", "Set preferences", "Set settings", "Set", "Sure", "Yes"],
+        ["Accept", "Agree", "Allow", "Approve", "Continue", "Got it", "I agree", "I understand", "OK", "Okay", "Save preferences", "Save settings", "Save", "Set preferences", "Set settings", "Set", "Sure", "Yes"],
         ["Alle", "Akzeptieren", "Annehmen", "Einverstanden", "Einwilligen", "stimme zu", "akzeptiere", "verstehe", "OK", "Okay", "Einstellungen speichern", "Speichern", "Sicher", "Ja", "Zustimmen", "Fortfahren"],
         ["Accetto", "Acconsento", "Continua", "Ho capito", "OK", "Okay", "Salva le preferenze", "Salva le impostazioni", "Salva", "Impostazioni salvate", "Impostazioni", "Imposta le preferenze", "Imposta le impostazioni", "Imposta", "Sicuro", "Si"],
         ["Aceptar", "Acepto", "Aprobar", "Continuar", "Entiendo", "OK", "Okay", "Guardar preferencias", "Guardar configuración", "Guardar", "Preferencias guardadas", "Configuración guardada", "Configuración", "Establecer preferencias", "Establecer configuración", "Establecer", "Seguro", "Sí"],
@@ -13,4 +14,23 @@ def find_cookie_match(button_text):
         for cookie_shorthand in cookie_list:
             if cookie_shorthand.lower() in button_text.lower():
                 return True
+    return False
+def find_cookie_match(driver):
+    buttons = driver.find_elements(By.TAG_NAME, "button")
+    # get the text description or innerHTML of all buttons
+    for button in buttons:
+        if accept_match(button.text):
+            return button
+
+    role_buttons = driver.find_elements(By.CSS_SELECTOR, "[role=button]")
+    for button in role_buttons:
+        print(button.text)
+        if accept_match(button.text):
+            return button
+    # find all elements with onclick attribute
+   
+    elements = driver.find_elements(By.XPATH, "//*[@onclick]")
+    for element in elements:
+        if accept_match(element.text):
+            return button
     return False
